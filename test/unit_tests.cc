2ad8805b350c114c33231740e4176c9ba69075ea
CNugteren <web@cedricnugteren.nl>
2015-10-12 17:18:38 +0200

    Added unit tests for Device and Context classes

--- test/unit_tests.cc
+++ test/unit_tests.cc
@@ -40,25 +40,121 @@
 
 // Settings
 const size_t kPlatformID = 0;
+const size_t kDeviceID = 0;
 
 // =================================================================================================
 
-// Tests all public methods of the Platform class
 SCENARIO("platforms can be created and used", "[Platform]") {
   GIVEN("An example platform") {
     auto platform = CLCudaAPI::Platform(kPlatformID);
     auto num_devices = platform.NumDevices();
 
-    #if USE_OPENCL
+    #if USE_OPENCL // Not available for the CUDA version
     WHEN("its underlying data-structure is retrieved") {
       auto raw_platform = platform();
-
       THEN("a copy of this platform can be created") {
         auto platform_copy = CLCudaAPI::Platform(raw_platform);
         REQUIRE(platform_copy.NumDevices() == num_devices);
       }
     }
     #endif
+
+    WHEN("a copy is created using the copy constructor") {
+      auto platform_copy = CLCudaAPI::Platform(platform);
+      THEN("the platform's properties remain unchanged") {
+        REQUIRE(platform_copy.NumDevices() == num_devices);
+      }
+    }
+  }
+}
+
+// =================================================================================================
+
+SCENARIO("devices can be created and used", "[Device][Platform]") {
+  GIVEN("An example device on a platform") {
+    auto platform = CLCudaAPI::Platform(kPlatformID);
+    auto device = CLCudaAPI::Device(platform, kDeviceID);
+
+    GIVEN("...and device properties") {
+      auto device_version = device.Version();
+      auto device_vendor = device.Vendor();
+      auto device_name = device.Name();
+      auto device_type = device.Type();
+      auto device_max_work_group_size = device.MaxWorkGroupSize();
+      auto device_max_work_item_dimensions = device.MaxWorkItemDimensions();
+      auto device_max_work_item_sizes = device.MaxWorkItemSizes();
+      auto device_local_mem_size = device.LocalMemSize();
+      auto device_capabilities = device.Capabilities();
+      auto device_core_clock = device.CoreClock();
+      auto device_compute_units = device.ComputeUnits();
+      auto device_memory_size = device.MemorySize();
+      auto device_memory_clock = device.MemoryClock();
+      auto device_memory_bus_width = device.MemoryBusWidth();
+
+      // TODO: test for valid device properties
+
+      WHEN("its underlying data-structure is retrieved") {
+        auto raw_device = device();
+        THEN("a copy of this device can be created") {
+          auto device_copy = CLCudaAPI::Device(raw_device);
+          REQUIRE(device_copy.Name() == device_name); // Only verifying device name
+        }
+      }
+
+      WHEN("a copy is created using the copy constructor") {
+        auto device_copy = CLCudaAPI::Device(device);
+        THEN("the device's properties remain unchanged") {
+          REQUIRE(device_copy.Name() == device_name); // Only verifying device name
+        }
+      }
+
+      WHEN("the local memory size is tested") {
+        THEN("the maximum local memory size should be considered valid") {
+          REQUIRE(device.IsLocalMemoryValid(device_local_mem_size) == true);
+        }
+        THEN("more than the maximum local memory size should be considered invalid") {
+          REQUIRE(device.IsLocalMemoryValid(device_local_mem_size+1) == false);
+        }
+      }
+
+      WHEN("the local thread configuration is tested") {
+        THEN("equal to the maximum size in one dimension should be considered valid") {
+          REQUIRE(device.IsThreadConfigValid({device_max_work_item_sizes[0],1,1}) == true);
+          REQUIRE(device.IsThreadConfigValid({1,device_max_work_item_sizes[1],1}) == true);
+          REQUIRE(device.IsThreadConfigValid({1,1,device_max_work_item_sizes[2]}) == true);
+        }
+        THEN("more than the maximum size in one dimension should be considered invalid") {
+          REQUIRE(device.IsThreadConfigValid({device_max_work_item_sizes[0]+1,1,1}) == false);
+          REQUIRE(device.IsThreadConfigValid({1,device_max_work_item_sizes[1]+1,1}) == false);
+          REQUIRE(device.IsThreadConfigValid({1,1,device_max_work_item_sizes[2]+1}) == false);
+        }
+      }
+    }
+  }
+}
+
+// =================================================================================================
+
+SCENARIO("contexts can be created and used", "[Context][Device][Platform]") {
+  GIVEN("An example context on a device") {
+    auto platform = CLCudaAPI::Platform(kPlatformID);
+    auto device = CLCudaAPI::Device(platform, kDeviceID);
+    auto context = CLCudaAPI::Context(device);
+
+    WHEN("its underlying data-structure is retrieved") {
+      auto raw_context = context();
+      THEN("a copy of this context can be created") {
+        auto context_copy = CLCudaAPI::Context(raw_context);
+        REQUIRE(context_copy() != nullptr);
+      }
+    }
+
+    WHEN("a copy is created using the copy constructor") {
+      auto context_copy = CLCudaAPI::Context(context);
+      THEN("its underlying data-structure is not null") {
+        REQUIRE(context_copy() != nullptr);
+      }
+    }
   }
 }
 
